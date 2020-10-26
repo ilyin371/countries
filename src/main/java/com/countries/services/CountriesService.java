c3e42da923ea38b1f05d685e41d83684318185e6
Vladimir Ilyin <vladimir.ilyin@neueda.com>
2020-10-26 08:00:00 +0200

    Refactor

--- src/main/java/com/countries/services/CountriesService.java
+++ src/main/java/com/countries/services/CountriesService.java
@@ -16,25 +16,30 @@ public class CountriesService {
     private final static int DEFAULT_LIMIT = 10;
 
     private final CountriesProvider provider;
+    private final RegionalBloc region = RegionalBloc.EU;
 
     public CountriesService(CountriesProvider provider) {
         this.provider = provider;
     }
 
     Flux<Country> getTopByPopulationDensity(Optional<Integer> limit) {
-        return provider.getCountries(RegionalBloc.EU)
+        return getCountries()
                 .sort(Comparator.comparing(Country::getPopulationDensity).reversed())
                 .take(limit.orElse(DEFAULT_LIMIT));
     }
 
     Flux<Country> findByCurrency(Currency currency) {
-        return provider.getCountries(RegionalBloc.EU)
+        return getCountries()
                 .filter(country -> country.getCurrencies().contains(currency));
     }
 
     Flux<Country> findByName(String namePattern) {
         val regex = "(?i)" + namePattern.replaceAll("\\*", ".*");
-        return provider.getCountries(RegionalBloc.EU)
+        return getCountries()
                 .filter(country -> country.getName().matches(regex));
+    }
+
+    private Flux<Country> getCountries() {
+        return provider.getCountries(region);
     }
 }
